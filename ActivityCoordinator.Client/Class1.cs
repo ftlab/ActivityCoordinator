a27889aef556d3c70eda926c14970f9d6c071281
ulyanov yuriy <ulyanov.yuriy@live.com>
2018-07-13 11:58:07 +0300

    sln structure

---
+++ ActivityCoordinator.Client/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ActivityCoordinator.Client
+{
+    public class Class1
+    {
+    }
+}
---
+++ ActivityCoordinator.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ActivityCoordinator.Core
+{
+    public class Class1
+    {
+    }
+}
